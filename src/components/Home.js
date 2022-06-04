3a371f8df0b6b9ec65ce8762e99c0c7c0f57b249
Narayane Ribeiro Medeiros <narayane.medeiros@ga.ita.br>
2022-06-04 20:46:34 -0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from "./Footer";
-import Header from "./Header";
+import Header from "./HeaderHome";
 import styled from 'styled-components';
 
 export default function Home(){
