1b5613a348035d525409f4fbf346c5a1f1d2a5ec
Narayane Ribeiro Medeiros <narayane.medeiros@ga.ita.br>
2022-06-04 21:15:51 -0300

    Update History.js

--- src/components/History.js
+++ src/components/History.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Footer from "./Footer";
 import Header from "./Header";
+import styled from 'styled-components';
 
 
 export default function History(){
     return(
-        <Header></Header>
-        <Footer></Footer>
+        <HistoryStyle>
+            <Header></Header>
+            <Footer></Footer>
+        </HistoryStyle>
     );
 }
+
+const HistoryStyle = styled.div`
+background: white;
+`;
