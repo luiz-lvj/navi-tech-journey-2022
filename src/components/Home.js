aafbcf73b2471e6fb1511c3814090be8c5f679e2
Bryan <bryanborck@gmail.com>
2022-06-05 11:57:03 -0300

    com cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import logoWhite from '../imgs/Logo_White.png';
+import ParkCard from "./ParkCard";
+import InfoCard from "./InfoCard";
 import Footer from "./Footer";
 import HeaderHome from "./HeaderHome";
 import styled from 'styled-components';
+import painelSolar1 from '../imgs/painel_solar1.png'
 
 export default function Home(){
     return(
         <HomeStyle>
-            
             <HeaderHome></HeaderHome>
+
+            <ParkCard
+            title ="title"
+            image={painelSolar1}
+            type ="type"
+            text="text"
+            value1="value1"
+            stat1="stat1"
+            value2="value2"
+            stat2="stat2"
+            />
+
+            <InfoCard
+            title ="title"
+            image={painelSolar1}
+            type ="type"
+            text="text"
+            />
             <Footer></Footer>
         </HomeStyle>
     );
@@ -22,6 +41,6 @@ const HomeStyle = styled.div`
     width: 100%;
     height: 740px;
 
-    background: #F0F0F0;
+    background: #F5F5F5;
         
 `;
