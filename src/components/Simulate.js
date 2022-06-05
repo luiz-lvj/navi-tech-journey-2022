4a81b21d2becbda730e34b00957b421769c7cdce
Narayane Ribeiro Medeiros <narayane.medeiros@ga.ita.br>
2022-06-04 21:18:09 -0300

    Update Simulate.js

--- src/components/Simulate.js
+++ src/components/Simulate.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Footer from "./Footer";
 import Header from "./Header";
+import styled from 'styled-components';
 
 
 export default function Simulate(){
     return(
-        <Header></Header>
-        <Footer></Footer>
+        <SimulateStyle>
+            <Header></Header>
+            <Footer></Footer>
+        </SimulateStyle>
     );
 }
+
+const SimulateStyle = styled.div`
+background: white;
+`;
