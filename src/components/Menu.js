8ca6f7a30cc7786cab8853fd45d78fa208ef043c
Narayane Ribeiro Medeiros <narayane.medeiros@ga.ita.br>
2022-06-04 21:50:47 -0300

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import Footer from "./Footer";
+import Header from "./Header";
+import styled from 'styled-components';
 
-export default function Home(){
+
+export default function Menu(){
     return(
-        <></>
+        <Box>
+        <MenuStyle>
+            <h2>
+                Perfil
+            </h2>
+        </MenuStyle>
+        <MenuStyle>
+            <h2>
+                Adicionar carteira
+            </h2>
+        </MenuStyle>
+        <MenuStyle>
+            <h2>
+            Configurações
+            </h2>
+        </MenuStyle>
+        <MenuStyle>
+            <h2>
+                Sair do perfil
+            </h2>
+        </MenuStyle>
+        <MenuStyle>
+            <h2>
+                Ajuda
+            </h2>
+        </MenuStyle>
+        
+        </Box>
     );
 }
+
+const Box = styled.div`
+padding-top:100px;
+background: #FFFFFF;
+width: 390px;
+height:789px;
+`;
+const MenuStyle = styled.div`
+background: #FFFFFF;
+border-bottom:1px solid #A800FF;
+width: 390px;
+height:55px;
+padding-top: 20px;
+text-align: center;
+color: #A800FF;
+font-style: italic;
+font-size:13px;
+`;
