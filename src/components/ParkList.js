fb88139da68e481361d20d6f042acbee1449632d
Narayane Ribeiro Medeiros <narayane.medeiros@ga.ita.br>
2022-06-04 21:16:47 -0300

    Update ParkList.js

--- src/components/ParkList.js
+++ src/components/ParkList.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Footer from "./Footer";
 import Header from "./Header";
+import styled from 'styled-components';
 
 
 export default function ParkList(){
     return(
-        <Header></Header>
-        <Footer></Footer>
+        <ParkListStyle>
+            <Header></Header>
+            <Footer></Footer>
+        </ParkListStyle>
     );
 }
+
+const ParkListStyle = styled.div`
+background: white;
+`;
